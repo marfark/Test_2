f568ca27d4521fce6c9e3a588c7252571faab1fd
Vinogradov Anton <antovio@yandex.ru>
2015-10-21 19:02:06 +0300

    first commit. Test 2. Yay!!!

---
+++ test2_1/src/by/testproject/anton/test2_1.java
@@ -0,0 +1,21 @@
+package by.testproject.anton;
+
+public class test2_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float f = 7.0F;
+		//char c = "z";
+		byte b = 127;
+		System.out.println("b = " + b);
+		
+		//boolean n = null;
+		
+		int j = 'a';
+		
+		System.out.println("the result of int j = 'a'. Then println(j): " + j);
+		
+		
+	}
+
+}
